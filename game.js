0ef150aaff4745fe5a06bc13a47ca6ceffe6f4ae
Justin Bradford <78094730+JBradford883@users.noreply.github.com>
2022-03-08 11:41:40 -0500

    Function to restart game after snake runs into itself or the wall

--- game.js
+++ game.js
@@ -1,8 +1,17 @@
 let lastRenderTime = 0
 
 function main(currentTime) {
-  const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000
+  if (gameOver) {
+    if (confirm('You lost. Press ok to restart')) {
+      window.location = '/'
+    }
+    return
+  }
+
+
   window.requestAnimationFrame(main)
+  const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000
+  if (secondsSinceLastRender < 1 / SNAKE_SPEED) return
   lastRenderTime = currentTime
   console.log(secondsSinceLastRender)
 }
