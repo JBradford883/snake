d7b844a1ffe11336051a54db0b5f43a34e027e7c
Justin Bradford <78094730+JBradford883@users.noreply.github.com>
2022-03-08 11:28:16 -0500

    import grid to make random grid position of the food

--- food.js
+++ food.js
@@ -1,4 +1,5 @@
 import { onSnake, expandSnake } from './snake.js'
+import { randomGridPosition } from './grid.js'
 const EXPANSION_RATE = 5
 function getRandomFoodPosition() {
   let newFoodPosition
