4e34330bd736f4892c80acfedc62c552e8a3db75
Justin Bradford <78094730+JBradford883@users.noreply.github.com>
2022-03-08 11:29:31 -0500

    Food position

--- food.js
+++ food.js
@@ -1,5 +1,7 @@
 import { onSnake, expandSnake } from './snake.js'
 import { randomGridPosition } from './grid.js'
+
+let food = getRandomFoodPosition()
 const EXPANSION_RATE = 5
 
 export function update() {
