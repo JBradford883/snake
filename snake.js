3f5ca96142e6861f758c6fb6260e7c3d2c49090a
Justin Bradford <78094730+JBradford883@users.noreply.github.com>
2022-03-08 11:32:16 -0500

    Draws snake on the game board

--- snake.js
+++ snake.js
@@ -3,3 +3,12 @@ import { getInputDirection } from "./input.js"
 export const SNAKE_SPEED = 5
 const snakeBody = [{ x: 11, y: 11 }]
 let newSegments = 0
+export function draw(gameBoard) {
+  snakeBody.forEach(segment => {
+    const snakeElement = document.createElement('div')
+    snakeElement.style.gridRowStart = segment.y
+    snakeElement.style.gridColumnStart = segment.x
+    snakeElement.classList.add('snake')
+    gameBoard.appendChild(snakeElement)
+  })
+}
