179a03b9a3d72bf949453502d0fcc1906e38f68c
Justin Bradford <78094730+JBradford883@users.noreply.github.com>
2022-03-08 11:29:18 -0500

    Function to draw the food on the game board

--- food.js
+++ food.js
@@ -9,6 +9,14 @@ export function update() {
   }
 }
 
+export function draw(gameBoard) {
+  const foodElement = document.createElement('div')
+  foodElement.style.gridRowStart = food.y
+  foodElement.style.gridColumnStart = food.x
+  foodElement.classList.add('food')
+  gameBoard.appendChild(foodElement)
+}
+
 function getRandomFoodPosition() {
   let newFoodPosition
   while (newFoodPosition == null || onSnake(newFoodPosition)) {
