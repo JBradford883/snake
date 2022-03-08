71437384068d241acfffdbf6cebf8c7f5d0787f5
Justin Bradford <78094730+JBradford883@users.noreply.github.com>
2022-03-08 11:28:46 -0500

    Export the expansion rate of the food

--- food.js
+++ food.js
@@ -1,6 +1,14 @@
 import { onSnake, expandSnake } from './snake.js'
 import { randomGridPosition } from './grid.js'
 const EXPANSION_RATE = 5
+
+export function update() {
+  if (onSnake(food)) {
+    expandSnake(EXPANSION_RATE)
+    food = getRandomFoodPosition()
+  }
+}
+
 function getRandomFoodPosition() {
   let newFoodPosition
   while (newFoodPosition == null || onSnake(newFoodPosition)) {
